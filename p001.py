214eb1bc4ee765e48dee357d6edafdbb7123a2bd
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2015-06-12 19:25:57 +0530

    p001

---
+++ p001.py
@@ -0,0 +1,12 @@
+"""Problem 001
+
+If we list all the natural numbers below 10 that are
+multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+ans = sum(n for n in range(1000) if n % 3 == 0 or n % 5 == 0)
+
+print(ans)
