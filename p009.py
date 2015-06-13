99279e78afc93a8b3fe1fb388e1943013c24b0b8
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2015-06-13 08:14:02 +0530

    p009

--- p009.py
+++ p009.py
@@ -9,12 +9,10 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
 
-a, b, c = next((a, b, c)
-               for a in range(1, 500)
-               for b in range(1, 500)
-               for c in range(1, 500)
-               if a < b < c and a**2 + b**2 == c**2 and a + b + c == 1000)
-
-ans = a * b * c
+ans = next(a * b * c
+           for a in range(1, 500)
+           for b in range(1, 500)
+           for c in range(1, 500)
+           if a < b < c and a**2 + b**2 == c**2 and a + b + c == 1000)
 
 print(ans)
